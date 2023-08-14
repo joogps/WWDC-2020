95a221dfa6801b86d3104d6f9a697112c5804968
João Gabriel <joogps@gmail.com>
2023-08-14 17:38:54 -0300

    Lower target

--- dash/dashApp.swift
+++ dash/dashApp.swift
@@ -13,6 +13,16 @@ struct dashApp: App {
         WindowGroup {
             ContentView()
         }
-        .windowResizability(.contentSize)
+        .windowResizabilityContentSize()
+    }
+}
+
+extension Scene {
+    func windowResizabilityContentSize() -> some Scene {
+        if #available(macOS 13.0, *) {
+            return windowResizability(.contentSize)
+        } else {
+            return self
+        }
     }
 }
